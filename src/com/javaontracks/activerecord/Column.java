04a3a2944f3f66feac8cb26567408a79dd055007
idrism <idrism@8a46e0f5-8f40-0410-af3b-bd2b07cc43de>
2007-12-05 21:17:17 +0000

    initial commit

---
+++ src/com/javaontracks/activerecord/Column.java
@@ -0,0 +1,20 @@
+package com.javaontracks.activerecord;
+
+public final class Column {
+	public final String name;
+	public Object defaultValue;
+	public final Class<?> type;
+	public boolean allowNulls;
+
+	public Column(String name, Class<?> columnClass) {
+		this.name = name;
+		this.type = columnClass;
+	}
+
+	public Column(String name, Object defaultValue, Class<?> columnClass, boolean allowNulls) {
+		this.name = name;
+		this.defaultValue = defaultValue;
+		this.type = columnClass;
+		this.allowNulls = allowNulls;
+	}
+}
