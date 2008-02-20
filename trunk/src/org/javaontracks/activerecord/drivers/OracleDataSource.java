1e01ee13f2ffb34a5055086145d611b4e047fad4
idrism <idrism@8a46e0f5-8f40-0410-af3b-bd2b07cc43de>
2008-02-20 19:46:29 +0000

    added OracleDataSource

---
+++ trunk/src/org/javaontracks/activerecord/drivers/OracleDataSource.java
@@ -0,0 +1,68 @@
+package org.javaontracks.activerecord.drivers;
+
+import oracle.jdbc.pool.OracleConnectionPoolDataSource;
+import javax.sql.DataSource;
+import java.util.Properties;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.io.PrintWriter;
+
+/**
+ * @author Idris
+ * requires oracle driver
+ */
+public class OracleDataSource implements DataSource {
+	OracleConnectionPoolDataSource source = null;
+	protected int maxConnections = 50;
+	protected int initialConnections = 5;
+
+	public OracleDataSource(String packageName, Properties props) throws SQLException {
+		source = new OracleConnectionPoolDataSource();
+		source.setDataSourceName("ActiveRecord-" + packageName);
+		source.setServerName(props.getProperty("serverName"));
+		source.setDatabaseName(props.getProperty("databaseName"));
+		source.setUser(props.getProperty("user"));
+		source.setPassword(props.getProperty("password"));
+		try {
+			maxConnections = Integer.parseInt(props.getProperty("maxConnections", "50"));
+		} catch (Exception ex) {
+		}
+		try {
+			initialConnections = Integer.parseInt(props.getProperty("initialConnections", "5"));
+		} catch (Exception ex) {
+		}
+		/*
+		 * should use a ConnectionPoolDataSource (like below) manually
+		 * source = new PGConnectionPoolDataSource();
+		 * source.setServerName(props.getProperty("serverName"));
+		 * source.setDatabaseName(props.getProperty("databaseName"));
+		 * source.setUser(props.getProperty("user"));
+		 * source.setPassword(props.getProperty("password"));
+		 * connections = new Vector<PooledConnection>();
+		 */
+	}
+
+	public Connection getConnection() throws SQLException {
+		return source.getConnection();
+	}
+
+	public Connection getConnection(String username, String password) throws SQLException {
+		return source.getConnection(username, password);
+	}
+
+	public PrintWriter getLogWriter() throws SQLException {
+		return source.getLogWriter();
+	}
+
+	public int getLoginTimeout() throws SQLException {
+		return source.getLoginTimeout();
+	}
+
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		source.setLogWriter(out);
+	}
+
+	public void setLoginTimeout(int seconds) throws SQLException {
+		source.setLoginTimeout(seconds);
+	}
+}
